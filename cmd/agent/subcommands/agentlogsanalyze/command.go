d725b1d3b4be83629df8650d329e5ffa48087124
andrewqian2001datadog <andrew.qian@datadoghq.com>
2024-10-23 15:30:11 -0400

    WIP

--- cmd/agent/subcommands/agentlogsanalyze/command.go
+++ cmd/agent/subcommands/agentlogsanalyze/command.go
@@ -8,21 +8,18 @@ package agentlogsanalyze
 
 import (
 	"bufio"
-	"bytes"
+	"context"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
-	"time"
 
 	"go.uber.org/fx"
 
 	"github.com/DataDog/datadog-agent/cmd/agent/command"
 	"github.com/DataDog/datadog-agent/comp/core"
 	"github.com/DataDog/datadog-agent/comp/core/config"
-	logComponent "github.com/DataDog/datadog-agent/comp/core/log"
 	log "github.com/DataDog/datadog-agent/comp/core/log/def"
-	"github.com/DataDog/datadog-agent/pkg/api/util"
+	"github.com/DataDog/datadog-agent/comp/logs"
 	pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 
@@ -40,10 +37,41 @@ type CliParams struct {
 
 const defaultLogFile = "/var/log/datadog/logs-agent.log"
 
+type Params struct {
+	DefaultLogFile string
+}
+
+func getSharedFxOption() fx.Option {
+	return fx.Options(
+		config.Module(),
+		logComponent,
+
+		// TODO: (components) - some parts of the agent (such as the logs agent) implicitly depend on the global state
+		// set up by LoadComponents. In order for components to use lifecycle hooks that also depend on this global state, we
+		// have to ensure this code gets run first. Once the common package is made into a component, this can be removed.
+		fx.Invoke(func(lc fx.Lifecycle) {
+			lc.Append(fx.Hook{
+				OnStart: func(ctx context.Context) error {
+					// Main context passed to components
+					common.MainCtx, common.MainCtxCancel = context.WithCancel(context.Background())
+
+					// create and setup the Autoconfig instance
+					common.LoadComponents(common.MainCtx, aggregator.GetSenderManager(), pkgconfig.Datadog.GetString("confd_path"))
+					return nil
+				},
+			})
+		}),
+		logs.Bundle,
+	)
+}
+
 // Commands returns a slice of subcommands for the 'agent' command.
 func Commands(globalParams *command.GlobalParams) []*cobra.Command {
 	cliParams := &CliParams{
 		GlobalParams: globalParams,
+	}
+	params := &Params{
+		DefaultLogFile: defaultLogFile,
 	}
 
 	cmd := &cobra.Command{
@@ -53,9 +81,11 @@ func Commands(globalParams *command.GlobalParams) []*cobra.Command {
 		RunE: func(_ *cobra.Command, _ []string) error {
 			return fxutil.OneShot(logsAnalyze,
 				fx.Supply(cliParams),
+				fx.Supply(params),
 				fx.Supply(command.GetDefaultCoreBundleParams(cliParams.GlobalParams)),
 				core.Bundle(),
-				fx.Supply(logComponent.LogForDaemon(string(loggerName), "log_file", params.DefaultLogFile)),
+				fx.Supply(log.ForDaemon("LOGS", "log_file", params.DefaultLogFile)),
+				getSharedFxOption(),
 			)
 		},
 	}
@@ -108,39 +138,6 @@ func logsAnalyze(logComponent log.Component, config config.Component, cliParams 
 			}
 		}
 	})
-}
-
-func streamRequest(url string, body []byte, duration time.Duration, onChunk func([]byte)) error {
-	var e error
-	c := util.GetClient(false)
-	if duration != 0 {
-		c.Timeout = duration
-	}
-	// Set session token
-	e = util.SetAuthToken(pkgconfigsetup.Datadog())
-	if e != nil {
-		return e
-	}
-
-	e = util.DoPostChunked(c, url, "application/json", bytes.NewBuffer(body), onChunk)
-
-	if e == io.EOF {
-		return nil
-	}
-	if e != nil {
-		fmt.Printf("Could not reach agent: %v \nMake sure the agent is running before requesting the logs and contact support if you continue having issues. \n", e)
-	}
-	return e
-}
-
-// openFileForWriting opens a file for writing
-func openFileForWriting(filePath string) (*os.File, *bufio.Writer, error) {
-	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return nil, nil, fmt.Errorf("error opening file %s: %v", filePath, err)
-	}
-	bufWriter := bufio.NewWriter(f) // default 4096 bytes buffer
-	return f, bufWriter, nil
 }
 
 // checkDirExists checks if the directory for the given path exists, if not then create it.
