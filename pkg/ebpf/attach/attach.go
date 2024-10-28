c0fcc72960678e0b7798f34c24dde92e4d42bf56
Bryce Kahle <bryce.kahle@datadoghq.com>
2024-10-28 10:09:55 -0700

    remove maxactive on < 4.12 kernels

--- pkg/ebpf/attach/attach.go
+++ pkg/ebpf/attach/attach.go
@@ -13,6 +13,7 @@ import (
 	"github.com/cilium/ebpf/link"
 
 	"github.com/DataDog/datadog-agent/pkg/ebpf/loader"
+	"github.com/DataDog/datadog-agent/pkg/util/kernel"
 )
 
 // Collection attaches all the programs in the collection
@@ -26,6 +27,19 @@ func Collection(coll *loader.Collection) (links []Link, err error) {
 
 	for name, kprobe := range coll.Kprobes {
 		if kprobe.IsReturnProbe {
+			if kprobe.Options != nil && kprobe.Options.RetprobeMaxActive != 0 {
+				kv, err := kernel.HostVersion()
+				if err != nil {
+					return nil, fmt.Errorf("kernel version: %s", err)
+				}
+				if kv < kernel.VersionCode(4, 12, 0) {
+					// kernels < 4.12 do not support maxactive
+					// added in https://github.com/torvalds/linux/commit/696ced4fb1d76802f864d8848aa4716633f83c17
+					// remove the option, otherwise cilium/ebpf will return an error
+					kprobe.Options.RetprobeMaxActive = 0
+				}
+			}
+
 			l, err := link.Kretprobe(kprobe.AttachTo, kprobe.Program, kprobe.Options)
 			if err != nil {
 				return nil, fmt.Errorf("link kretprobe %s to %s: %s", name, kprobe.AttachTo, err)
