d49ab40abdaceeed52ced9e3a0d62f888e1c8c8d
kovags <marcio.kovags@datadoghq.com>
2025-04-02 18:36:59 +0200

    Testing a fake eBPF heap with unions

--- pkg/security/ebpf/c/include/helpers/network/raw.h
+++ pkg/security/ebpf/c/include/helpers/network/raw.h
@@ -5,7 +5,8 @@
 
 __attribute__((always_inline)) struct raw_packet_event_t *get_raw_packet_event() {
     u32 key = 0;
-    return bpf_map_lookup_elem(&raw_packet_event, &key);
+    union union_heap_t* uh = bpf_map_lookup_elem(&union_heap, &key);
+    return &uh->raw_packet_event;
 }
 
 #endif
